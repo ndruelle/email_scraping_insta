99c261602b22bf41a2ed6ad29d310c206f98944e
Nicolas Druelle <ndruelle@thestorefront.com>
2020-05-12 13:11:08 -0400

    version working, get_username plus get_data, yay

--- Get_Username.py
+++ Get_Username.py
@@ -14,6 +14,7 @@ with open('settings.json','r') as settings:
 chromedriver_path = '/usr/local/bin/chromedriver'
 webdriver = webdriver.Chrome(chromedriver_path)
 
+selected_user = []
 
 def login():
     #Open the instagram login page
@@ -50,40 +51,80 @@ def login():
 
 def get_username():
 
-    selected_user = []
-    print(selected_user)
-
     for hashtag in hashtags :
-        for x in range(0,10):
-            webdriver.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
-            sleep (5)
 
-            # Get the first post thumbnail and click on it
-            first_thumbnail = webdriver.find_element_by_xpath (
-                '//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div')
+        webdriver.get ('https://www.instagram.com/explore/tags/' + hashtag + '/')
+        sleep (5)
 
-            first_thumbnail.click ()
-            sleep(random.randint (1, 3))
+        # Get the first post thumbnail and click on it
+        first_thumbnail = webdriver.find_element_by_xpath (
+            '//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div')
+
+        first_thumbnail.click ()
+        sleep (random.randint (1, 3))
+
+        for x in range (1,3):
+
 
             try:
-                username = webdriver.find_element_by_xpath(
-                    '/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[1]').text
-
+                username = webdriver.find_element_by_xpath ('/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[1]').text
             except:
-                username = webdriver.find_element_by_xpath(
-                    '/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[1]/a').text
+                username = webdriver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[1]/a').text
 
             selected_user.append(username)
-    return selected_user
+
+            webdriver.find_element_by_link_text ('Next').click ()
+            sleep (random.randint (2, 4))
+
+        print(selected_user)
 
 
-def get_data(username_insta, password_insta, selected_users):
+def get_data(username_insta, password_insta):
+    users = selected_user
     instagram = Instagram()
     instagram.with_credentials (username_insta, password_insta, 'chache')
     instagram.login ()
 
-    for user in selected_users:
-        account = instagram.get_account(user)
+    data = {'id': [],
+            'Username': [],
+            'Full name': [],
+            'Biography': [],
+            'Profile pic url': [],
+            'External Url': [],
+            'Number of followers': [],
+            'Number of follows': [],
+            'Is private': [],
+            'Is verified': [],
+            'Number of published posts': [],
+            'Business address': [],
+            'Business email': [],
+            'Business category': [],
+            'Business phone number': [],
+            'Joined recently': [],
+            'Is Business Account': []
+            }
+
+    for user in users:
+        account = instagram.get_account (user)
+        data['id'].append (account.identifier)
+        data['Username'].append (account.username)
+        data['Full name'].append (account.full_name)
+        data['Biography'].append (account.biography)
+        data['Profile pic url'].append (account.profile_pic_url)
+        data['External Url'].append (account.external_url)
+        data['Number of followers'].append (account.followed_by_count)
+        data['Number of follows'].append (account.follows_count)
+        data['Is private'].append (account.is_private)
+        data['Is verified'].append (account.is_verified)
+        data['Number of published posts'].append (account.media_count)
+        data['Business address'].append (account.business_address_json)
+        data['Business email'].append (account.business_email)
+        data['Business category'].append (account.business_category_name)
+        data['Business phone number'].append (account.business_phone_number)
+        data['Joined recently'].append (account.is_joined_recently)
+        data['Is Business Account'].append (account.is_business_account)
+
+    print(data)
 
 
 login()
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from igramscraper.instagram import Instagram
+import json
 
 instagram = Instagram()
 #proxies = {'http': 'http://123.45.67.8:1087','https': 'http://123.45.67.8:1087'}
@@ -7,8 +8,51 @@ instagram = Instagram()
 instagram.with_credentials('nicolas.druelle@ieseg.fr','rsk7b9252594','chache')
 instagram.login()
 
+selected_users = ['kauflokal','nicolas_drll']
 
-#medias = instagram.get_medias_by_tag('kauflokal', count=20)
+data = {'id':[],
+        'Username': [],
+        'Full name': [],
+        'Biography': [],
+        'Profile pic url': [],
+        'External Url': [],
+        'Number of followers': [],
+        'Number of follows': [],
+        'Is private': [],
+        'Is verified': [],
+        'Number of published posts': [],
+        'Business address': [],
+        'Business email': [],
+        'Business category': [],
+        'Business phone number': [],
+        'Joined recently': [],
+        'Is Business Account': []
+        }
 
-account = instagram.get_account('kauflokal')
-print(account)
+for user in selected_users:
+    account = instagram.get_account(user)
+    data['id'].append(account.identifier)
+    data['Username'].append(account.username)
+    data['Full name'].append(account.full_name)
+    data['Biography'].append(account.biography)
+    data['Profile pic url'].append(account.profile_pic_url)
+    data['External Url'].append(account.external_url)
+    data['Number of followers'].append (account.followed_by_count)
+    data['Number of follows'].append(account.follows_count)
+    data['Is private'].append (account.is_private)
+    data['Is verified'].append (account.is_verified)
+    data['Number of published posts'].append (account.media_count)
+    data['Business address'].append(account.business_address_json)
+    data['Business email'].append (account.business_email)
+    data['Business category'].append (account.business_category_name)
+    data['Business phone number'].append (account.business_phone_number)
+    data['Joined recently'].append(account.is_joined_recently)
+    data['Is Business Account'].append(account.is_business_account)
+
+
+
+
+
+
+print(data)
+
